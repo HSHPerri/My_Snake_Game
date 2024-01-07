b76156d9aa9bb63ea9e35d65d6c2a741519d1bcc
Harry Perriton <harryjperriton@gmail.com>
2024-01-07 22:19:44 +0000

    Added logic for wall collisions.

--- main.py
+++ main.py
@@ -8,9 +8,14 @@ import time
 from snake import Snake
 from snake_food import Food
 from scoreboard import Scoreboard
+from turtle import Turtle
 
 # Calls a new screen object locally and a new "snake" object from the snake class.
 screen = Screen()
+end_message = Turtle()
+end_message.hideturtle()
+end_message.penup()
+end_message.color("white")
 
 # Screen setup variables, set screen resolution, background colour and title.
 screen.setup(width=600, height=600)
@@ -42,9 +47,22 @@ while game_is_on:
 
     snake.move_snake()  # This moves the snake forward continuously.
 
+    # If snake head collides with a food spot, update its position and player score.
     if snake.snake_head.distance(food) < 15:
         food.move_food()
         scoreboard.update_score()
         snake.new_segment()
+
+    # If snake collides with a wall, stop the snake moving and give game over message.
+    # Had to separate out into two if statements otherwise it only recognises two walls as death pointers.
+    # First group checks the X coordinate.
+    elif snake.snake_head.position()[0] == 300 or snake.snake_head.position()[0] == -300:
+        game_is_on = False
+        end_message.write("Game Over.\nClick to exit.", False, "center", ('Arial', 24, 'normal'))
+
+    # Second group checks the y coordinate.
+    elif snake.snake_head.position()[1] == 300 or snake.snake_head.position()[1] == -300:
+        game_is_on = False
+        end_message.write("Game Over.\nClick to exit.", False, "center", ('Arial', 24, 'normal'))
 
 screen.exitonclick()
